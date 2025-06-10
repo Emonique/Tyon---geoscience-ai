55e7ba5eb5ad40507105816c78420d4b68d2b6a9
Emonique <85489511+Emonique@users.noreply.github.com>
2025-06-10 03:03:51 +0100

    Update dashboard_app.py

--- Tyon geoscience AI/dashboard/dashboard_app.py
+++ Tyon geoscience AI/dashboard/dashboard_app.py
@@ -1,3 +1,23 @@
+# Check and install required packages
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+required = {
+    'matplotlib': 'matplotlib',
+    'pandas': 'pandas',
+    'numpy': 'numpy'
+}
+
+for package, import_name in required.items():
+    try:
+        __import__(import_name)
+    except ImportError:
+        install(package)
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
